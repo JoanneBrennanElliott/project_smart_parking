afeeab6bfe5326fc7a7ba28f529f25e97a88501e
JoanneBrennanElliott <x23410001@student.ncirl.ie>
2026-06-22 19:47:38 +0100

    Removed the double up of code for disabled parking space booking functionality
    Removed the double up of code for disabled parking space booking functionality

--- client_browser.js
+++ client_browser.js
@@ -424,25 +424,4 @@ document.getElementById("stopBtn").addEventListener("click", () => {
 
 
 
-//Disabled Parking Space
-document.getElementById("bookDisabledBtn").addEventListener("click", () => {
-    fetch("http://localhost:3000/bookDisabledSpace", 
-	{ method: "POST" })
-        .then(res => res.json())
-        .then(data => {
-		const type = data.success ? "success" : "error";
-//		updateStatus("Disabled Space: " + data.message, "success");
-			updateStatus(
-                `Disabled Space: ${data.message}
-				\n User: ${data.userId}
-				\n Space: ${data.spaceId} 
-				\n Time: ${data.timestampExpiry}`, type);
-        })      
-        .catch(err => {
-            updateStatus("Error booking disabled space", "error");
-            console.error(err);
-        });
-});
-
-
 
